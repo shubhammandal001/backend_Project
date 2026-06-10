01546f661e8f4a6f60b2c34778bb72872ae4cefc
shubham mandal <shubhammandal13579@gmail.com>
2026-06-10 07:51:59 +0530

    aggregate piplines and sub aggregate piplines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {apiError} from "../utils/apiError.js";
 import {User} from "../models/user.models.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async(userId) => {
     try {
@@ -336,6 +337,131 @@ const updateUserCoverImage = asyncHandler(async(req,res) => {
     )
 })
 
+const getUserChannelProfile = asyncHandler (async(req,res) => {
+   const {username} = req.params
+
+   if (!username?.trim()) {
+    throw new apiError(400,"username is missing")
+   }
+
+   const channel = await User.aggregate([
+    {
+        $match:{
+            username:username?.toLowerCase()
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"channel",
+            as:"subscribers"
+        }
+    },
+    {
+        $lookup:{
+            from:"subscriptions",
+            localField:"_id",
+            foreignField:"subscriber",
+            as:"subscribedTo"
+        }
+    },
+    {
+        $addFields:{
+            subscribersCount:{
+                $size: "$subscribers"
+            },
+            channelSubscribedToCount :{
+                $size: "$subscribedTo"
+            },
+            isSubscribed :{
+                $cond:{
+                    if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                    then: true,
+                    else:false
+                }
+            }
+        }
+    },
+    {
+        $project:{
+            fullname:1,
+            username:1,
+            avatar:1,
+            coverImage:1,
+            email:1,
+            isSubscribed :1,
+            channelSubscribedToCount:1,
+            subscribersCount:1
+
+        }
+    }
+   ])
+   // console.log(channel);
+
+   if (!channel?.length) {
+        throw new apiError(404,"channel doesn't exist !")
+   }
+
+   return res
+   .status(200)
+   .json(
+    new apiResponse(404,channel[0],"channel fetched successfully!")
+   )
+   
+
+})
+
+const getWatchHistory = asyncHandler (async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id : new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200,user[0].watchHistory,"watchHistory fetched successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
@@ -346,4 +472,5 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
+    getUserChannelProfile,
 };
